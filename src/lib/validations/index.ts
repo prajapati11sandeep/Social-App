96fcb3f76e5d036e20710a26392083fe7b74a514
sandeep111995 <56584522+prajapati11sandeep@users.noreply.github.com>
2023-11-26 13:41:27 +0530

    Project setup and signup with appwrite

---
+++ src/lib/validations/index.ts
@@ -0,0 +1,8 @@
+import * as z from "zod";
+
+export const SignupValidation = z.object({
+  name: z.string().min(2, { message: 'Name must be at least 2 characters' }),
+  username: z.string().min(2, { message: 'Name must be at least 2 characters' }).max(50),
+  email: z.string().email(),
+  password: z.string().min(8, { message: 'Password must be min 8 characters' })
+});
